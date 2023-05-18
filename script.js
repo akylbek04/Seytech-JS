b5520e6d5438e5caac217d73196fa5a6a4c8e8b3
Akylbek04 <zamirovakylbek920@gmail.com>
2023-05-18 22:39:50 +0200

    completed

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const a = Math.floor(Math.random() * 5)+ 1;
+const b = prompt("Your love -> ");
+const c = prompt("Dream place ->");
+const d = prompt("Dream career ->");
+
+console.log("You will be a " +d+ " in " +c+ ", and married to "+b+" with "+a+" kids.");
